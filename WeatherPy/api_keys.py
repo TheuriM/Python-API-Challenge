6003e9c4a9c923b604ea2092dc1027ddcd04cc5b
git config --global user.name your_username <125322343+TheuriM@users.noreply.github.com>
2024-09-08 20:53:16 -0500

    updated the code

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,6 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key =  "f6991238b411006aefcdc4e775361c0e"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "3378229d3c1f4f2db554d74290b6b356"
+
